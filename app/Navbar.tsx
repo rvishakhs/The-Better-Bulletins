10483612f6ea0ffc9d174b663af89b688b21c7ce
rvishakhs <rvishakhs@gmail.com>
2022-12-21 23:49:56 +0000

    errors and errors

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,16 +1,23 @@
+// "use client";
+
 import React from 'react'
 import {categories} from "../constants/index"
 import Navlink from './Navlink'
+// import { usePathname } from 'next/navigation';
 
 type Props = {}
 
 const category = ["business", "finance", "sports", "tech", "commerce"]
 
 function Navbar({}: Props) {
+
+    // const pathname = usePathname()
+    // console.log(pathname);
+    
   return (
-    <div className='grid grid-cols-4 my-6 mx-6 items-center justify-center gap-4 md:mx-16 md:grid-cols-7 text-xs md:text-sm'>
+    <div className='grid grid-cols-4 my-2 mx-6 items-center align-middle gap-4 md:mx-16 md:grid-cols-7 text-xs md:text-sm'>
         {categories.map((category) => (
-            <Navlink key={category} category={category} />
+            <Navlink key={category} category={category} isActive={true} />
         ))}
     </div>
   )
--- app/Navlink.tsx
+++ app/Navlink.tsx
@@ -3,18 +3,20 @@ import React from 'react'
 
 type Props = {
     category : string
+    isActive: boolean
 }
 
-function Navlink({category}: Props) {
+function Navlink({category, isActive}: Props) {
   return (
       <div className='grid grid-cols-4 md:grid-col-7'>
         <a
             href={`/${category}`}
-            className="navlink"    
+            className={`navlinks ${isActive && "underline decoration-blue-400 underline-offset-4 text-lg"} `}  
         >
                 {category}
         </a>
              
+
     </div>
   )
 }
